6d066edc366beb9b0b6e76033e68cf76ed8510cb
zloyleva <zloyleva.imt@gmail.com>
2019-07-20 12:43:36 +0300

    set fillabale for News

--- laravel/app/Http/Controllers/NewsController.php
+++ laravel/app/Http/Controllers/NewsController.php
@@ -41,7 +41,7 @@ class NewsController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\News  $news
+     * @param  \App\Models\News  $news
      * @return \Illuminate\Http\Response
      */
     public function show(News $news)
@@ -52,7 +52,7 @@ class NewsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\News  $news
+     * @param  \App\Models\News  $news
      * @return \Illuminate\Http\Response
      */
     public function edit(News $news)
@@ -64,7 +64,7 @@ class NewsController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\News  $news
+     * @param  \App\Models\News  $news
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, News $news)
@@ -75,7 +75,7 @@ class NewsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\News  $news
+     * @param  \App\Models\News  $news
      * @return \Illuminate\Http\Response
      */
     public function destroy(News $news)
--- laravel/app/Models/News.php
+++ laravel/app/Models/News.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    //
+    protected $fillable = [
+        'title', 'slug', 'body', 'thumbnail', 'views', 'likes', 'published_status'
+    ];
+
 }
